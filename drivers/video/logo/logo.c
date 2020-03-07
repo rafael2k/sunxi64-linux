0c7bf0b11ef4972ac3e99ae12dd70b051c902304
Arnaud Ferraris <arnaud.ferraris@collabora.com>
2020-03-07 17:06:24 +0100

    video: logo: add Mobian logo for PinePhone

--- drivers/video/logo/logo.c
+++ drivers/video/logo/logo.c
@@ -75,6 +75,10 @@ const struct linux_logo * __ref fb_find_logo(int depth)
 		/* Generic Linux logo */
 		logo = &logo_linux_clut224;
 #endif
+#ifdef CONFIG_LOGO_MOBIAN_CLUT224
+		/* Mobian logo */
+		logo = &logo_mobian_clut224;
+#endif
 #ifdef CONFIG_LOGO_DEC_CLUT224
 		/* DEC Linux logo on MIPS/MIPS64 or ALPHA */
 		logo = &logo_dec_clut224;
